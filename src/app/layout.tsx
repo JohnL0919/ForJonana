6cc76bb88dbc69d9a6ef54a950623d643f14ed79
John <johnlin8968@gmail.com>
2026-01-28 15:36:07 +1100

    matcha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Jonana Re-subscription",
   description: "For the love of my life",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍵</text></svg>",
+  },
 };
 
 export default function RootLayout({
